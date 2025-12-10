2748861c56338db8b3a1e632002a1eeac4f62df0
santhiyaraju18 <santhiyaraju155@gmail.com>
2025-12-10 16:49:50 +0530

    Add files via upload

---
+++ PropsExample.jsx
@@ -0,0 +1,9 @@
+export default function PropsExample(props)
+{
+    return<>
+    <h1>Props Example</h1>
+    <p>{props.title}</p>
+    <p>{props.tutor}</p>
+    </>
+
+}
---
+++ Welcome.jsx
@@ -0,0 +1,4 @@
+export default function Welcome()
+{
+    return(<h1>hai san</h1>);
+}
